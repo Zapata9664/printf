a075090c7f410c5630aebb1ec7ed0a5f4fa24446
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-24 17:31:04 -0500

    adding printf

--- _printf.c
+++ _printf.c
@@ -3,40 +3,37 @@
 #include <stdlib.h>
 #include "main.h"
 
-int (*search (const char *format))(va_list)
+int (*search(const char *format))(va_list)
 {
-estructura arr[] = {
-("c", printf_character);
-("s", printf_string);
-(NULL, NULL);
+    estructura arr[] = {
+        ("c", printf_character);
+    ("s", printf_string);
+    (NULL, NULL);
 };
 
-int i = 0; 
+int i = 0;
 
 for (i; arr[i].s; i++)
 {
     if (*format == arr[i].s)
     {
-    break;
+        break;
     }
     return (arr[i].f);
 }
 }
 
-
 int _printf(const char *format, ...)
 {
     int i = 0;
     int counter;
-for (i; format[i] != '%'; i++)
-{
-write(1,&c,1);
-counter++;
-}
-}
 
-int main (void)
-{
-_printf("hola");
-return (0);
+    for (i; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            _putchar(format[i]);
+            counter++;
+        }
+    }
 }
