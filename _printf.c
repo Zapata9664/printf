014a5f4a21d7471aa3eaaa8702ec946244a2cb7d
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-24 16:58:00 -0500

    adding printf

--- _printf.c
+++ _printf.c
@@ -1,32 +1,42 @@
-#include "main.h"
 #include <stdarg.h>
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int (*search (const char *format))(va_list)
+{
+estructura arr[] = {
+("c", printf_character);
+("s", printf_string);
+(NULL, NULL);
+};
+
+int i = 0; 
+
+for (i; arr[i].s; i++)
+{
+    if (*format == arr[i].s)
+    {
+    break;
+    }
+    return (arr[i].f);
+}
+}
+
 
 int _printf(const char *format, ...)
 {
-    char string = 0;
-    /*char copy = 0;*/
+    int i = 0;
+    int counter;
+for (i; format[i] != '%'; i++)
+{
+write(1,&c,1);
+counter++;
+}
+}
 
-    if ( format != NULL)
-    {
-        va_list str; /* create the argument*/
-        /*va_list copy;  this the copy of argument*/
-
-        va_start(str, format); /* init the pointer*/
-        /*va_copy(copy, str);  this the copy of the pointer*/
-
-        string += va_arg(str, int); /*send the position of pointer*/
-        /*copy = va_arg(copy, ); the copy of the position of pointer*/
-
-        va_end(str); /*finished the arguments*/
-        /*va_end(copy); finished the argument*/
-    }
-
-    if (format == NULL)
-    {
-        return (0);
-    }
-
-    return(string);
-    /*return(copy);*/
+int main (void)
+{
+_printf("hola");
+return (0);
 }
--- main.h
+++ main.h
@@ -2,5 +2,13 @@
 #define MAIN_H
 
 int _putchar(char c);
+int _printf(const char *format, ...);
+
+typedef struct original
+{
+    char *s;
+    int (*f)(va_list);
+} estructura;
+
 
 #endif /*MAIN_H*/
