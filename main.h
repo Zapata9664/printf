f7e16c0ce844cf43f3986eed163227d582f187d6
felipeholbi <4758@holbertonschool.com>
2022-03-25 15:26:37 -0500

    main

--- main.h
+++ main.h
@@ -1,17 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
-
-int _putchar(char c);
-int _printf(const char *format, ...);
-int printf_character(va_list i);
 
 typedef struct original
 {
-    char s;
-    int (f)(va_list);
+    char *s;
+    int (*f)(va_list);
 } estructura;
 
+int (*search(char format))(va_list);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int printf_character(va_list i);
+int printf_string(va_list s);
+int _strcmp(char *s1, char *s2);
 
-#endif /MAIN_H/
+#endif /*MAIN_H*/
