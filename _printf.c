5b1d1fb08484e02b8e3c347cd925d22bc4b229ef
felipeholbi <4758@holbertonschool.com>
2022-03-28 07:22:57 -0500

    printf

--- _printf.c
+++ _printf.c
@@ -6,27 +6,12 @@ int (*search(char c))(va_list)
     estructura arr[] = {
         {"c", printf_character},
         {"s", printf_string},
-<<<<<<< HEAD
-=======
-      /*  {"s", printf_string}, */
->>>>>>> 83e1bbd24576251f1e42d7f3da202496bb9aed9a
         {NULL, NULL}};
 
     int i = 0;
     char *x = &c;
 
     for (i; arr[i].s != NULL; i++)
-<<<<<<< HEAD
-=======
-    {
-        if (strcmp(format, arr[i].s) == 0)
-        {
-            return (arr[i].f);
-        }
-    }
-    return(NULL);
-    for (i; arr[i].s; i++)
->>>>>>> 83e1bbd24576251f1e42d7f3da202496bb9aed9a
     {
         if (_strcmp(arr[i].s, x) == 0)
         {
@@ -53,26 +38,8 @@ int _printf(const char *format, ...)
         else
         {
             i++;
-<<<<<<< HEAD
             f = search(format[i]);
             counter += (f) ? f(list) : _printf("%%%c", format[i]);
-=======
-            if (format[i] != '\0')
-            {
-                printf("%c", format[i]);
-                f = search(&format[i]);
-                printf("%c", format[i]);
-                counter += f(list);
-            if (format[i + 1] != '\0')
-            {
-                f = search(&format[i + 1]);
-                if (f != NULL)
-                {
-                    f(list);
-                }
-            }
-            
->>>>>>> 83e1bbd24576251f1e42d7f3da202496bb9aed9a
         }
         i++;
     }
@@ -86,8 +53,4 @@ int main(void)
     _printf("%c\n", ch);
     printf("%c\n", ch);
     return (0);
-<<<<<<< HEAD
-=======
-        va_end(list);
->>>>>>> 83e1bbd24576251f1e42d7f3da202496bb9aed9a
 }
